8144fd0ebc9ae95ac243aed82d8bcdbe64e79c11
kubrafelek <kbr.flk@hotmail.com>
2023-02-15 08:53:09 +0300

    Last files added

--- src/main/java/com/kubrafelek/security/config/JwtService.java
+++ src/main/java/com/kubrafelek/security/config/JwtService.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 import java.security.Key;
 import java.util.Date;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.function.Function;
 
